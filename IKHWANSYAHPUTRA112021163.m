05fe35ff05cec611157f9b43de11d5784d246b04
ikhwansyahputra123 <149784709+ikhwansyahputra123@users.noreply.github.com>
2023-11-03 04:04:33 -0700

    Add files via upload

---
+++ IKHWANSYAHPUTRA112021163.m
@@ -0,0 +1,88 @@
+disp('NAMA : Ikhwansyah Putra-112021163-KELAS A')
+disp('4.1. UNTUK MATRIK YANG DINYATAKAN DALAM PERSAMAAN (4.2) DAN (4.3) BERIKUT')
+disp('4.2')
+A= [ 1 2 3; 2 1 1; 3 2 1]
+disp('4.3')
+B= [ 4 4 5; 6 1 2; 3 5 5]
+disp('a')
+det (A)
+det (B)
+disp('b')
+size (A)
+size (B)
+disp('c')
+trace (A)
+trace (B)
+disp('d')
+norm (A)
+norm (B)
+disp('e')
+C = A + B
+disp('f')
+C = A - B
+disp('g')
+C = A * B
+disp('h')
+C = A.*B
+disp('i')
+C = A^2
+disp('j')
+C = A.^2
+disp('k')
+T = A'
+T = B'
+disp('l')
+C = A./B
+disp('m')
+C = A.\B
+disp('n')
+C = A/B
+disp('o')
+inv (A)
+inv (B)
+disp('p')
+C = null (A)
+disp('q')
+C = orth (A)
+disp('r')
+C = rref (A)
+disp('s')
+C = eig (A)
+C = eig (B)
+disp('t')
+C = svd (A)
+C = svd (B)
+disp('u')
+S = triu (A)
+S = triu (B)
+disp('v')
+S = tril (A)
+S = tril (B)
+disp('w')
+S = max (A)
+S = max (B)
+disp('x')
+S = min (A)
+S = min (B)
+disp('y')
+S = sum (A)
+S = sum (B)
+disp('z')
+S = diag (A)
+S = diag (B)
+
+disp('4.2. DENGAN MENGGUNAKAN MATLAB, BUATLAH')
+disp('a')
+I = eye (5,5)
+disp('b')
+I = zeros (3,3)
+disp('c')
+I = ones (4,4)
+disp('d')
+I = rand (3,4)
+disp('e')
+I = randn (3,4)
+
+
+
+
